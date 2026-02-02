5e785ca6668efc415d01ce23ae2b0ceaf4ffa9a9
Nancy Bankson <nbankson3@gmail.com>
2026-02-01 18:28:27 -0600

    Updated authorization for getRecipes

--- src/utils/recipes-api.ts
+++ src/utils/recipes-api.ts
@@ -2,23 +2,26 @@ import axios from "axios";
 
 export async function getRecipes() {
   try {
-    console.log(`${import.meta.env.VITE_API_URL}/api/recipes`);
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/recipes`);
-
-    if (!response.ok) throw new Error("API Error! Response was not ok.");
-    const data = await response.json();
-    return data;
+    let token = localStorage.getItem("token");
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/recipes`, {
+      headers: {
+        'Authorization': token
+      }
+    });
+    return response.data; // The retrieved data
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching post:', error);
   }
 }
 
 export async function login(email: string, password: string) {
   try {
+    console.log(`${import.meta.env.VITE_API_URL}/api/users/login`);
     const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/login`, {
       email: email,
       password: password
     });
+    localStorage.setItem("token", response.data.token);
     console.log('Logged in', response.data);
   } catch (error) {
     console.error('Error logging in', error);
